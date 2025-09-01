0e487193055bc62af8ede6e65f9aad5e213d6624
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-09-01 11:14:52 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,2 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {content:['./index.html','./src/**/*.{ts,tsx,js,jsx}'],theme:{extend:{fontFamily:{sans:['Inter','ui-sans-serif','system-ui','Segoe UI','Roboto','Arial','sans-serif']}}},plugins:[]}
